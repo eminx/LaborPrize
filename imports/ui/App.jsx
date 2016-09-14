f9e3817e9eeb5c59b2db2b36f9cb3ea8c88504b1
Emin Durak <e10durak@gmail.com>
2016-09-14 23:47:08 +0200

    started the project

---
+++ imports/ui/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+ 
+// App component - represents the whole app
+export default class App extends Component {
+ 
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Labor Prize</h1>
+        </header> 
+      </div>
+    );
+  }
+}
