467c4078b3ce5ccca276b754967de45c8e7ed2ef
Aymen4 <Aymen@Aymens-Mac.lan>
2016-09-25 16:30:13 +0200

    added JoinLaborPrize with a login

--- imports/ui/pages/JoinLaborPrize.jsx
+++ imports/ui/pages/JoinLaborPrize.jsx
@@ -1,14 +1,83 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
 // App component - represents the whole app
-export default class CreateTask extends Component {
+export default class JoinLaborPrize extends Component {
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(e.target)
+        var ele = $(e.target);
+        var companyName = ele.find("#company_name").val();
+        var email = ele.find("#email").val();
+        var username = ele.find("#username").val();
+        var password = ele.find("#password").val();
+        var confirmPassword = ele.find("#confirmPassword").val();
+
+        if (password === confirmPassword && password !== "" && confirmPassword !== "") {
+
+            var accountInfo = {
+                companyName: companyName,
+                username: username,
+                password: password
+            };
+            Accounts.createUser(accountInfo, function (er) {
+                if (er) {
+                    Materialize.toast('Error!', 4000);
+                }
+                else {
+                    Meteor.loginWithPassword(username, password, function (er) {
+                        if (er) {
+                            Materialize.toast('Error!', 4000);
+                        } else {
+                            console.log("success");
+                        }
+
+                    })
+                }
+            });
+        } else {
+            Materialize.toast('passwords don¨t match!', 4000) // 4000 is the duration of the toast
+
+        }
+    }
 
     render() {
         return (
-            <div className="container">
-                <header>
-                    <h1>Join Labor-Prize</h1>
-                </header>
+            <div className="row">
+                <form onSubmit={this.onSubmit} className="col offset-s4 s4">
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="company_name" type="text" class="validate"/>
+                            <label htmlFor="company_name">Company Name</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="email" type="text" class="validate"/>
+                            <label htmlFor="email">Email</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="username" type="text" class="validate"/>
+                            <label htmlFor="username">Username</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="password" type="password" class="validate"/>
+                            <label htmlFor="password">Password</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="confirmPassword" type="password" class="validate"/>
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <button className="waves-effect waves-light btn">Join</button>
+                    </div>
+                </form>
             </div>
         );
     }
--- imports/ui/pages/StartPage.jsx
+++ imports/ui/pages/StartPage.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-
 // App component - represents the whole app
 export default class StartPage extends Component {
 
+
+    handleJoinLaborPrize(e){
+        e.preventDefault();
+    }
     render() {
         return (
             <div className="container">
                 <header>
                     <h1>Main Page</h1>
                 </header>
-            </div>
+                    <form onSubmit = {this.handleJoinLaborPrize}>
+                        <button>Login</button>
+                        <br/>
+                        <br/>
+                        <button>Join Labor-prize</button>
+                    </form>
+                </div>
         );
     }
 }
